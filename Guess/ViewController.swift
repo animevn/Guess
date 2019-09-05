61f497a9cf8fc0b7cac187221bdb3ee23afbb79f
animevn <haanhth21@gmail.com>
2019-09-05 08:11:07 +0700

    prepare IB

--- Guess/ViewController.swift
+++ Guess/ViewController.swift
@@ -2,11 +2,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sdChoose: UISlider!
+    @IBOutlet weak var lbMin: UILabel!
+    @IBOutlet weak var lbMax: UILabel!
+    @IBOutlet weak var lbReturn: UILabel!
+    @IBOutlet weak var lbStep: UILabel!
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
 
+    @IBAction func bnRestart(_ sender: UIButton) {
+    }
+    
+    @IBAction func bnCredit(_ sender: UIButton) {
+    }
+    
+    
+    
 
 }
 

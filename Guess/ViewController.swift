d6370c5af76cfe0985f84abb2bf153caeb765c1f
animevn <haanhth21@gmail.com>
2019-09-05 10:48:33 +0700

    done all

--- Guess/ViewController.swift
+++ Guess/ViewController.swift
@@ -13,9 +13,14 @@ class ViewController: UIViewController {
     private var max = 100
     private var step = 0
     private var target = 0
+    private var input = 0
     
     private func setButtonTitle(){
         bnOK.setTitle("OK", for: .normal)
+    }
+    
+    @objc private func onSliderMove(){
+        input = Int(sdChoose.value)
     }
     
     private func drawSlider(){
@@ -31,6 +36,8 @@ class ViewController: UIViewController {
         let rightImage = UIImage(named: "sliderTrackerRight")
         let rightTrack = rightImage!.resizableImage(withCapInsets: inset)
         sdChoose.setMaximumTrackImage(rightTrack, for: .normal)
+        
+        sdChoose.addTarget(self, action: #selector(onSliderMove), for: .valueChanged)
     }
     
     private func setupViews(){
@@ -39,28 +46,67 @@ class ViewController: UIViewController {
         target = Int(arc4random_uniform(101))
         step = 0
         lbReturn.text = ""
+        lbMin.text = "0"
+        lbMax.text = "100"
         lbStep.text = "Step  \(step)"
+        sdChoose.minimumValue = 0
+        sdChoose.maximumValue = 100
         sdChoose.value = 50
+        input = Int(sdChoose.value)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setButtonTitle()
-        drawSlider()
         setupViews()
+        drawSlider()
     }
     
-
-    @IBAction func bnRestart(_ sender: UIButton) {
+    private func showAlertIfGuessRight(){
+        let alert = UIAlertController(
+            title: "Wow",
+            message: "You get number \"\(input)\" right in just \(step) steps",
+            preferredStyle: .alert)
         
+        let action = UIAlertAction(title: "OK", style: .default, handler: {action in
+            self.setupViews()
+        })
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    private func processInput(){
+        lbReturn.text = ""
+        if input == target{
+            showAlertIfGuessRight()
+        }else if input > target{
+            step += 1
+            lbReturn.text = "You've chosen \(input), which is wrong 😂"
+            max = input
+            lbMax.text = "\(input)"
+            sdChoose.maximumValue = Float(input)
+            lbStep.text = "\(step)"
+        }else if input < target{
+            step += 1
+            lbReturn.text = "You've chosen \(input), which is wrong 😂"
+            min = input
+            lbMin.text = "\(input)"
+            sdChoose.minimumValue = Float(input)
+            lbStep.text = "\(step)"
+        }
+        
+    }
+    
+    @IBAction func bnOk(_ sender: UIButton) {
+        processInput()
+    }
+    
+    @IBAction func bnRestart(_ sender: UIButton) {
+        setupViews()
     }
     
     @IBAction func bnCredit(_ sender: UIButton) {
         performSegue(withIdentifier: "credit", sender: self)
     }
-    
-    
-    
-
 }
 

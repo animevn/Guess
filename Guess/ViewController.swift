c0663cc8b99f6370d43dd1f7f8f7aea3f554fcbc
animevn <haanhth21@gmail.com>
2019-09-05 07:23:55 +0700

    prepare source

--- Guess/ViewController.swift
+++ Guess/ViewController.swift
@@ -1,18 +1,10 @@
-//
-//  ViewController.swift
-//  Guess
-//
-//  Created by haanh on 9/5/19.
-//  Copyright © 2019 haanh. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
 

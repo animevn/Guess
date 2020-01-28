98158b538283d7a26e15e026efd7362bc49c2dfd
animevn <haanhth21@gmail.com>
2020-01-28 23:08:32 +0700

    reset

--- Guess/CreditController.swift
+++ Guess/CreditController.swift
@@ -2,9 +2,5 @@ import UIKit
 
 class CreditController:UIViewController{
     
-    @IBOutlet weak var bnBack: UIButton!
-    
-    @IBAction func bnBack(_ sender: UIButton) {
-        dismiss(animated: true, completion: nil)
-    }
+   
 }
